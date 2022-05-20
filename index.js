d3fb96770e9047d656d34894f6c90fc424ff6d0e
Sabiha Bary <sabihaab9@gmail.com>
2022-05-20 14:26:44 -0400

    commit tree f84c4eec71b3944d13a7755dde2e6d5908b8dbf6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+
+  let messages = [];
+
+  for (let i=0; i < names.length; i++){
+  
+    messages.push("Thank you, " +  names[i] + ", for the wonderful surprise gift!");
+
+  }
+
+  return messages;
+}
+
+
+function countDown(positiveInteger){
+
+  while(positiveInteger >= 0){
+    // positiveInteger = positiveInteger -1; 
+    console.log(positiveInteger--);
+  }
+}
